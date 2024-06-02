1176770f741c01c6f0186cb3f52e2ca73287c985
SymonBezerra <simaobezerratranslate@gmail.com>
2024-06-02 19:36:04 -0300

    removed debug backgrounds

--- app.vue
+++ app.vue
@@ -11,11 +11,11 @@
             </div>
           </div>
           <!-- main content -->
-          <main class="col-span-12 bg-red-500 sm:col-span-8 lg:col-span-6">
-            asd
+          <main class="col-span-12 sm:col-span-8 lg:col-span-6">
+            <h1 class="text-white">main content</h1>
           </main>
           <!-- right sidebar -->
-          <div class="hidden md:block md:col-span-3 xl:col-span-4 bg-blue-500">
+          <div class="hidden md:block md:col-span-3 xl:col-span-4">
             <div class="sticky top-0">
               <SidebarRight />
             </div>
